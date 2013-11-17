94032e18dfd77417be2ac622cab3d1ef2a193d43
Maher Gamal <maher.gamal@orange.com>
2013-11-17 16:50:40 +0200

    Moved the english labels to property files.

--- src/main/java/tdd/EnglishNumberNameRepository.java
+++ src/main/java/tdd/EnglishNumberNameRepository.java
@@ -1,59 +1,36 @@
 package tdd;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.util.Properties;
 
 public class EnglishNumberNameRepository implements NumberNameRepository {
 
-    private Map<Integer, String> names = new HashMap<Integer, String>() {
-        {
-            put(0, "");
-            put(1, "one");
-            put(2, "two");
-            put(3, "three");
-            put(4, "four");
-            put(5, "five");
-            put(6, "six");
-            put(7, "seven");
-            put(8, "eight");
-            put(9, "nine");
-            put(10, "ten");
-            put(11, "eleven");
-            put(12, "twelve");
-            put(13, "thirteen");
-            put(14, "fourteen");
-            put(15, "fifteen");
-            put(16, "sixteen");
-            put(17, "seventeen");
-            put(18, "eighteen");
-            put(19, "nineteen");
-            put(20, "twenty");
-            put(30, "thirty");
-            put(40, "fourty");
-            put(50, "fifty");
-            put(60, "sixty");
-            put(70, "seventy");
-            put(80, "eighty");
-            put(90, "ninety");
+    private Properties props = new Properties();
+
+    public EnglishNumberNameRepository() {
+        try {
+            props.load(getClass().getResourceAsStream("/numbers_en.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    };
+    }
 
     @Override
     public String nameOf(int number) {
-        return names.get(number);
+        return props.getProperty(new Integer(number).toString());
     }
 
     @Override
     public String scaleOf(int number) {
         int numberOfDigits = new Integer(number).toString().length();
         if (numberOfDigits == 3) {
-            return "hundred";
+            return props.getProperty("hundred");
         }
         if (numberOfDigits >= 4 && numberOfDigits <= 6) {
-            return "thousand";
+            return props.getProperty("thousand");
         }
         if (numberOfDigits >= 7 && numberOfDigits <= 9) {
-            return "million";
+            return props.getProperty("million");
         }
         return "";
     }
